bba5750ce412bfec7c0a0fab93f575e007fbe618
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-10-26 21:03:51 +0530

    Adding the nav files

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useTheme } from '@mui/material/styles'; // Import the useTheme hook
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const theme = useTheme(); // Use the useTheme hook to get the theme object
+
+  return (
+    <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' style={{ flexGrow: 1 }}>
+            AcademiaX
+          </Typography>
+          <Button color='inherit' style={{ marginLeft: theme.spacing(2) }}>
+            Login
+          </Button>
+          <Button color='inherit' style={{ marginLeft: theme.spacing(2) }}>
+            Sign Up
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Navbar;
