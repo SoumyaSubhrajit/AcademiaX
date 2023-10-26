fa06e1dfcf4e3a2b7159e287969793c14c1c00ac
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-10-26 19:57:08 +0530

    Adding components in the code base

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './App.css';
-
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <Header />
       <h1>AcademiaX</h1>
+      <Footer />
     </div>
   )
 }
 
 export default App;
-// Tree.
